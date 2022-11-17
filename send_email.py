62e8e089fd41e31008fd00fbe1b48c391152c622
HQY <13191099631@163.com>
2022-11-17 13:50:34 +0800

    修改为国内的邮箱

--- send_email.py
+++ send_email.py
@@ -2,8 +2,8 @@ from email.mime.text import MIMEText
 import smtplib
 
 def send_email(email,height,average_height,count):
-    from_email = "happyfinch7788@gmail.com"
-    from_password = 'pomwpbtqxcbvgvwm'
+    from_email = "13191099631@163.com"
+    from_password = 'MURCDIOKRHQATKQA'
     to_email = email
 
     subject = "Height data"
@@ -13,9 +13,10 @@ def send_email(email,height,average_height,count):
     msg['Subject'] = subject
     msg['To'] = to_email
     msg['From'] = from_email
-
-    gmail = smtplib.SMTP('smtp.gmail.com',587)
-    gmail.ehlo()
-    gmail.starttls()
-    gmail.login(from_email,from_password)
-    gmail.send_message(msg)
+    # 由于gmail邮箱由于网络问题发不出去 改成用国内的邮箱发送
+    mail163 = smtplib.SMTP_SSL('smtp.163.com',994) # 实例化smtp 可以用SSL连接 也可以不用
+    # gmail.ehlo()
+    # gmail.starttls()
+    mail163.login(from_email,from_password)
+    mail163.sendmail(from_email,to_email,msg.as_string())
+    mail163.close()
